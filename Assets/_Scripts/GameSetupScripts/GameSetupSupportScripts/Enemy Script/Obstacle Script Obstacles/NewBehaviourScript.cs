a2f5837ed1629818ada3b7b869c6b21130dc0a1d
HaidenKJ <haiden.kaymenjoseph@gmail.com>
2025-04-10 23:05:46 -0400

    Obstacles mostly working?

--- Assets/_Scripts/GameSetupScripts/GameSetupSupportScripts/Enemy Script/Obstacle Script Obstacles/NewBehaviourScript.cs
+++ Assets/_Scripts/GameSetupScripts/GameSetupSupportScripts/Enemy Script/Obstacle Script Obstacles/NewBehaviourScript.cs
@@ -2,24 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AstroidScript : MonoBehaviour
+public class JumpingObstacle : MonoBehaviour
 {
-    [SerializeField] private float AstroidMoveSpeed = 5f;
+    [SerializeField] private float ObstacleMoveSpeed = 5f;
     [SerializeField] private float spawnXPosition = 8f; // Spawn position on the X-axis
     [SerializeField] private float despawnXPosition = -10f; // X position for despawning
-    // [SerializeField] private Animator asteroidAnimator; // Reference to the Animator component
 
-    private float randomYPosition;
-
-    void Start()
+    private void Start()
     {
-        SetRandomSpawnPosition();
+        SetFixedSpawnPosition();
     }
 
-    void Update()
+    private void Update()
     {
         // Move leftward
-        transform.Translate(Vector3.left * AstroidMoveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.left * ObstacleMoveSpeed * Time.deltaTime);
 
         // Respawn if off-screen
         if (transform.position.x <= despawnXPosition)
@@ -28,24 +25,20 @@ public class AstroidScript : MonoBehaviour
         }
     }
 
-    public void SetRandomSpawnPosition()
+    public void SetFixedSpawnPosition()
     {
-        randomYPosition = Random.Range(-4f, 4f); // Adjust vertical range as needed
-        transform.position = new Vector3(spawnXPosition, randomYPosition, 0f);
+        transform.position = new Vector3(spawnXPosition, -0.07f, 0f); // Always spawn at Y = -0.07f
     }
 
     public void TeleportToSpawn()
     {
-        SetRandomSpawnPosition();
+        SetFixedSpawnPosition();
     }
 
-    // Triggered when the asteroid collides with the player (using trigger event)
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        // Check if the collision is with the player
         if (collider.CompareTag("Player"))
         {
-            // Call the player's TakeDamage method
             PlayerScript player = collider.GetComponent<PlayerScript>();
             if (player != null)
             {
